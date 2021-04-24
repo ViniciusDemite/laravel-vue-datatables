46ea72f0d5aa43b001cb15821f0e617661502954
Vinicius Demite Gubolin <gubolin88@gmail.com>
2021-04-24 15:33:20 -0300

    componente campo de pesquisa

---
+++ resources/js/components/SearchField.vue
@@ -0,0 +1,20 @@
+<template>
+    <input
+        @change="searchStudents($event.target.value)"
+        type="search"
+        class="form-control"
+        placeholder="Search by name,email,phone,or address..."
+    />
+</template>
+
+<script>
+export default {
+    name: 'SearchField',
+    
+    methods: {
+        searchStudents(value) {
+            this.$emit('input', value)
+        }
+    }
+}
+</script>
